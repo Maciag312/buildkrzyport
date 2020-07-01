052e5380d6982dd2c0ba3410856f265fe357d417
Maciag312 <Maciag312>
2020-07-01 11:58:08 +0200

    build v

---
+++ app-build/precache-manifest.9f0b92d5aed13f40e18b0a7a4010e8e2.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1207e609c8f308ff995824ba9c97510",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01fda268a794f6625fe0",
+    "url": "/static/css/2.1a70fde6.chunk.css"
+  },
+  {
+    "revision": "783e7e8ca5a606c3da86",
+    "url": "/static/css/main.0fdf0f41.chunk.css"
+  },
+  {
+    "revision": "01fda268a794f6625fe0",
+    "url": "/static/js/2.fe010588.chunk.js"
+  },
+  {
+    "revision": "844ebc06925dfa3e0a3d7deef72e7b4c",
+    "url": "/static/js/2.fe010588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "783e7e8ca5a606c3da86",
+    "url": "/static/js/main.300372a9.chunk.js"
+  },
+  {
+    "revision": "6209db5d8b44797dabdc",
+    "url": "/static/js/runtime-main.71d64be9.js"
+  },
+  {
+    "revision": "8621b613a38b415ada70a97ddfcc0827",
+    "url": "/static/media/3dsmaxICON.8621b613.jpg"
+  },
+  {
+    "revision": "3dc4f3a35c791a56c321fc9a65cf853e",
+    "url": "/static/media/Abasenkolor-min.3dc4f3a3.jpg"
+  },
+  {
+    "revision": "1b3ca54657130b28b38db11a1bd576f6",
+    "url": "/static/media/Akonkurs1kolor-min.1b3ca546.jpg"
+  },
+  {
+    "revision": "96ad0d04052fae55c225f7931a3b950c",
+    "url": "/static/media/Akonkurs2kolor-min.96ad0d04.jpg"
+  },
+  {
+    "revision": "9f0276025c3078df571818938a81e8f2",
+    "url": "/static/media/Aurbanistykakolor-min.9f027602.jpg"
+  },
+  {
+    "revision": "4d77cd37cafa4abbd86a296ad93602a8",
+    "url": "/static/media/KRZYSIEKczb-min.4d77cd37.jpg"
+  },
+  {
+    "revision": "86b2bc9adac8c538ee9f59a52fb6648f",
+    "url": "/static/media/closeup1kolor-min.86b2bc9a.jpg"
+  },
+  {
+    "revision": "c3cf5a20d793da279c6d52aa72b5db4b",
+    "url": "/static/media/closeup2kolor-min.c3cf5a20.jpg"
+  },
+  {
+    "revision": "a049fb2c3a65344f1b6ffd81f6e9e22c",
+    "url": "/static/media/exteriorkolor-min.a049fb2c.jpg"
+  },
+  {
+    "revision": "08aea0d4556a8112fa957ed1b049aa7f",
+    "url": "/static/media/konkursARKxSITE_optimized.08aea0d4.jpg"
+  },
+  {
+    "revision": "32b206a0e8496ab8afc33279fcabb178",
+    "url": "/static/media/konkursPeacePavillion_optimized.32b206a0.jpg"
+  },
+  {
+    "revision": "ed1bae36ee3b16b4f240c6eb82ecbf68",
+    "url": "/static/media/mazdakolor-min.ed1bae36.jpg"
+  },
+  {
+    "revision": "9356e8c72267c662698745c1b850f21a",
+    "url": "/static/media/meksykkolor-min.9356e8c7.jpg"
+  },
+  {
+    "revision": "47c129e3d38cde478c73f9115275e7a9",
+    "url": "/static/media/newyorkkolor-min.47c129e3.jpg"
+  },
+  {
+    "revision": "dafbf23c8fe34d879b20d17f8be530e3",
+    "url": "/static/media/pływalnia_optimized.dafbf23c.jpg"
+  },
+  {
+    "revision": "77866858bbee2f3df94d6386eb8ae79a",
+    "url": "/static/media/rocakolor-min.77866858.jpg"
+  },
+  {
+    "revision": "47c979b04c88297f5d3f9ecf25645676",
+    "url": "/static/media/telaviv1kolor-min.47c979b0.jpg"
+  },
+  {
+    "revision": "ed51ece820ebfb02963521fe0151bd2d",
+    "url": "/static/media/telaviv2kolor-min.ed51ece8.jpg"
+  },
+  {
+    "revision": "08d2698ede9cecf678660d1d2f0183be",
+    "url": "/static/media/urbanistyka1_optimized.08d2698e.jpg"
+  },
+  {
+    "revision": "61434503549d0d769d7d3eb88e2b772b",
+    "url": "/static/media/urbanistyka2_optimized.61434503.jpg"
+  },
+  {
+    "revision": "581ed8621eb7990426cedf7496f2b713",
+    "url": "/static/media/urbanistyka3_optimized.581ed862.jpg"
+  },
+  {
+    "revision": "b8b7090c35dfd630a5e9e84b44f8a146",
+    "url": "/static/media/urbanistykaALL-min.b8b7090c.jpg"
+  }
+]);
